4bd8e7e6cb1c8ad0d923efa8d17a1730692acf25
Syed Galib <syedgalib.dev@outlook.com>
2020-02-01 16:20:04 +0600

    First deploy

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var slider_1 = new PlasmaSlider({
+  containerID: "plasma-slider-1",
+  width: 16,
+  height: 9,
+  showThumbnails: true,
+  // blurBackground: false,
+  // backgroundColor: '#000'
+  /* images: [
+    {
+      src: "https://images.unsplash.com/photo-1566640241039-2443899336c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+      alt: ""
+    },
+    {
+      src: "https://images.unsplash.com/photo-1570121962635-3552954619a6?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+      alt: ""
+    },
+    {
+      src: "https://images.unsplash.com/photo-1569834381156-7b735e41e57d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80",
+      alt: ""
+    },
+    {
+      src: "https://images.unsplash.com/photo-1547738765-ee82a7f07da8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+      alt: ""
+    },
+    {
+      src: "https://images.unsplash.com/photo-1567845531350-79f5c0464b96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+      alt: ""
+    }
+  ] */
+});
+slider_1.init();
